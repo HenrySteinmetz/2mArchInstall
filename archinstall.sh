6b236ae5debd009a31dcc118d52e3a16302b45a7
Henry <matteo.steinmetz@t-online.de>
2023-05-19 22:33:27 +0200

    changed exports

--- archinstall.sh
+++ archinstall.sh
@@ -2,11 +2,6 @@
 # This script asks for general install type and initializes startup
 # Discord: Handy#1684
 
-set -a
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-SCRIPTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/scripts
-CONFIGS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/configs
-PACKAGES_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/packages
-set +a
-
+export INSTALL_SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/scripts/install
+export PREINSTALL_SCRIPTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/scripts/preinstall
 ./scripts/preinstall/disks.sh
