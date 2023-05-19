0446a5c834e2d62f9415a48d6585ed79da08897d
Henry <matteo.steinmetz@t-online.de>
2023-05-19 22:37:43 +0200

    fixed increadibly dumm mistake

--- scripts/install/disks.sh
+++ scripts/install/disks.sh
@@ -18,9 +18,9 @@ then
   umount -A --recursive /mnt
   sgdisk -Z ${Disk}
   sgdisk -a 2048 -o ${Disk}
-  sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${Disk} # partition 1 (BIOS Boot Partition)
-  sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${Disk} # partition 2 (UEFI Boot Partition)
-  sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${Disk} # partition 3 (Root), default start, remaining
+  sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' /dev/${Disk} # partition 1 (BIOS Boot Partition)
+  sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' /dev/${Disk} # partition 2 (UEFI Boot Partition)
+  sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' /dev/${Disk} # partition 3 (Root), default start, remaining
   if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
     sgdisk -A 1:set:2 ${Disk}
   fi
