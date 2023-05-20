a52fd65d935e710eda82deb5f877ce928205f624
HenrySteinmetz <hsteinm2@lspb.de>
2023-05-20 13:49:33 +0200

    troubleshooting commit

--- scripts/install/prechroot/chroot.sh
+++ scripts/install/prechroot/chroot.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 # Discord: Handy#1684
 # Github: https://github.com/HenrySteinmetz
-echo "export AurHelper=${AurHelper}\nexport Bootloader=${BootLoader}\nexport Browser=${Browser}\nexport DesktopEnviorment=${DesktopEnviorment}\nexport InstallType=${InstallType}\n export KeyboardLayout=${KeyboardLayout}\nexport Timezone=${Timezone}\nexport Hostname=${Hostname}\nexport UserName=${UserName}\nexport Password=${Password}" >> ${INSTALL_SCRIPT_DIR}/postchroot/config.sh
+echo "AurHelper=${AurHelper}" 
+echo "BootLoader=${BootLoader}" 
+echo "Browser=${Browser}"
+echo "DesktopEnviorment=${DesktopEnviorment}"
+echo "InstallType=${InstallType}"
+echo "KeyboardLayout=${KeyboardLayout}"
+echo "Timezone=${Timezone}"
+echo "Hostname=${Hostname}"
+echo "UserName=${UserName}"
+echo "Password=${Password}"
 
-cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
-mkdir -p /mnt/root/2mArchInstall
-cp -r ${INSTALL_SCRIPT_DIR}/postchroot/ /mnt/root/2mArchInstall
-pacstrap -K /mnt base linux linux-firmware zsh
-arch-chroot /mnt /root/2mArchInstall/postchroot/init.sh
+#cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+#mkdir -p /mnt/root/2mArchInstall
+#cp -r ${INSTALL_SCRIPT_DIR}/postchroot/ /mnt/root/2mArchInstall
+#pacstrap -K /mnt base linux linux-firmware zsh
+#arch-chroot /mnt /root/2mArchInstall/postchroot/init.sh
