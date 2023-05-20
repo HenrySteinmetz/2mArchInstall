066d55e04ff3de22d9704eb679225592fb0fafab
HenrySteinmetz <hsteinm2@lspb.de>
2023-05-20 13:22:56 +0200

    added chmod +x to every file

--- archinstall.sh
+++ archinstall.sh
@@ -5,6 +5,25 @@
 export INSTALL_SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/scripts/install
 export PREINSTALL_SCRIPTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/scripts/preinstall
 
+
+chmod +x scripts/preinstall/dependencies.sh
+chmod +x scripts/preinstall/disks.sh
+chmod +x scripts/preinstall/locale.sh
+chmod +x scripts/preinstall/bootloader.sh
+chmod +x scripts/preinstall/installtype.sh
+chmod +x scripts/preinstall/aurhelper.sh
+chmod +x scripts/preinstall/browser.sh
+chmod +x scripts/preinstall/de.sh
+chmod +x scripts/install/prechroot/chroot.sh
+chmod +x scripts/install/prechroot/disks.sh
+chmod +x scripts/insatll/postchroot/config.sh
+chmod +x scripts/insatll/postchroot/graphicsdriver.sh
+chmod +x scripts/insatll/postchroot/init.sh
+chmod +x scripts/insatll/postchroot/locale.sh
+chmod +x scripts/insatll/postchroot/pacman.sh
+chmod +x scripts/insatll/postchroot/ucode.sh
+chmod +x scripts/insatll/postchroot/user.sh
+
 ./scripts/preinstall/dependencies.sh
 ./scripts/preinstall/disks.sh
 ./scripts/preinstall/locale.sh
