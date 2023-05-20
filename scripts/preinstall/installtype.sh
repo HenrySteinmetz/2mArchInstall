3e46edf347d36f8cfcfb20f93cd2a73d19c4df8d
HenrySteinmetz <hsteinm2@lspb.de>
2023-05-20 14:33:36 +0200

    fixed random typo

--- scripts/preinstall/installtype.sh
+++ scripts/preinstall/installtype.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 clear
 echo "Choose your desired install type"
-select installtype in full minimal xorg
+select install_type in full minimal xorg
 do
-  case $installtype in 
+  case $install_type in 
     full|minimal|xorg)
-      echo "InstallType=${InstallType}" >> ${SCRIPT_DIR}/vars.sh
+      echo "InstallType=${install_type}" >> ${SCRIPT_DIR}/vars.sh
       ./scripts/preinstall/aurhelper.sh
       ./scripts/preinstall/de.sh
       ./scripts/preinstall/browser.sh
