0985f00315082218c7d7b262eccbce4e77656172
HenrySteinmetz <hsteinm2@lspb.de>
2023-05-20 13:34:44 +0200

    i cant write chroot and actually added required folder

--- scripts/install/prechroot/chroot.sh
+++ scripts/install/prechroot/chroot.sh
@@ -4,6 +4,7 @@
 echo "export AurHelper=${AurHelper}\nexport Bootloader=${BootLoader}\nexport Browser=${Browser}\nexport DesktopEnviorment=${DesktopEnviorment}\nexport InstallType=${InstallType}\n export KeyboardLayout=${KeyboardLayout}\nexport Timezone=${Timezone}\nexport Hostname=${Hostname}\nexport UserName=${UserName}\nexport Password=${Password}" >> ${INSTALL_SCRIPT_DIR}/postchroot/config.sh
 
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+mkdir -p /mnt/root/2mArchInstall
 cp -r ${INSTALL_SCRIPT_DIR}/postchroot/ /mnt/root/2mArchInstall
 pacstrap -K /mnt base linux linux-firmware zsh
-arch-choot /mnt /root/2mArchInstall/init.sh
+arch-chroot /mnt /root/2mArchInstall/init.sh
