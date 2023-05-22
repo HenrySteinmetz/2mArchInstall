a04bc0942f2871547d7f2b0c557da9fd422e834b
HenrySteinmetz <hsteinm2@lspb.de>
2023-05-22 17:20:00 +0200

    fixed wrong case statement

--- scripts/preinstall/installtype.sh
+++ scripts/preinstall/installtype.sh
@@ -4,12 +4,15 @@ echo "Choose your desired install type"
 select install_type in full minimal xorg
 do
   case $install_type in 
-    full|minimal|xorg)
+    full)
       echo "InstallType=${install_type}" >> ${SCRIPT_DIR}/vars.conf
       ./scripts/preinstall/aurhelper.sh
       ./scripts/preinstall/de.sh
       ./scripts/preinstall/browser.sh
       break
+    ;;
+    minmal|xorg)
+      echo "InstallType=${install_type}" >> ${SCRIPT_DIR}/vars.conf
     ;;
     *)
       echo "Not a valid option"
